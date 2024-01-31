42f36a0fc016efa7e972b53f58fb3af33c87efa1
e-eberhardt <elenarose.ak@gmail.com>
2024-01-30 20:22:20 -0900

    elena test again

--- test.script.R
+++ test.script.R
@@ -1,5 +1,18 @@
 #I am testing stuffff
 
+install.packages("usethis")
 
+library(usethis)
 
+use_git_config(user.name = "e-eberhardt", user.email = "elenarose.ak@gmail.com")
 #I am trying to learn how to use how to use this thing 
+
+#Sabre is the best and fluffiest cat
+
+meow<- c("MEOWWWW", "mewaaaawww","wahahahahaaaaaaa", "purrrrr")
+tuna<- c("MEOWWWWWWWWWW")
+print(meow)
+
+print(tuna)
+
+git config --global user.name "e-eberhardt"
